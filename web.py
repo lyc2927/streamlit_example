74e60eb0819e235e3d3b6b7d821cd979ce4cb198
昌changＣ <91516060+lyc2927@users.noreply.github.com>
2023-07-29 21:20:26 +0800

    Add files via upload

---
+++ web.py
@@ -0,0 +1,23 @@
+import time
+import streamlit as st
+
+st.set_page_config(
+    page_title=" APP DEMO　",
+    page_icon="🧊2689",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
+
+# 设置网页标题
+st.header('2023ML Medicine')
+
+
+
+st.info('Hello world!')
+
+
